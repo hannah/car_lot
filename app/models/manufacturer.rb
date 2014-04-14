257e980a11a3b6ba2fdb850a3f19f0c00b63e291
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-14 14:27:47 -0400

    changed dependent destroy to dependent nullify

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,5 +2,5 @@ class Manufacturer < ActiveRecord::Base
   validates :country, presence: true
   validates :name, presence: true
 
-  has_many :cars, dependent: :destroy
+  has_many :cars, dependent: :nullify
 end
